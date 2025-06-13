50a444a79c328e88577ab83728981240fe243041
SalvatoreDL01 <104557779+SalvatoreDL01@users.noreply.github.com>
2025-06-13 11:31:32 +0200

    Aggiunta analisi sull'utilità dei local bridge nella rete

---
+++ statistics/local_bridges.py
@@ -0,0 +1,18 @@
+import networkx as nx
+import math
+
+from utils.utils import log_experiment, assign_cost_attributes, ceil_division
+
+G = nx.read_edgelist("../data/facebook_combined.txt", nodetype=int)
+
+local_bridges = nx.local_bridges(G)
+set_LB = set(local_bridges)
+print("Numero di local bridge nella rete: ", len(set_LB))
+print("Numero di bridge nella rete: ", sum(1 for e in set_LB if e[2] == float('inf')))
+
+# Salvataggio edge list del grafo senza i bridge
+G_removed = G.copy()
+for t in set_LB:
+    G_removed.remove_edge(t[0], t[1])
+
+nx.write_edgelist(G_removed, "../data/no_bridge.txt", comments='#', delimiter=' ', data=True, encoding='utf-8')
