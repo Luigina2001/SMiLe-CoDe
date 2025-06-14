7c586c7491903e8b1409b2ad9f82969fe2fd013e
Luigina Costante <luiginacostante@gmail.com>
2025-06-14 16:47:55 +0200

    Fix gestione dei costi uguali a zero nell'algoritmo CSG

--- algorithms/CSG.py
+++ algorithms/CSG.py
@@ -4,10 +4,17 @@ import networkx as nx
 from tqdm import tqdm
 import time
 
-from utils.utils import log_experiment, assign_cost_attributes, ceil_division
+import os
+import sys
+
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+if project_root not in sys.path:
+    sys.path.insert(0, project_root)
+
+from utils.utils import log_experiment, assign_cost_attributes, ceil_division  # noqa
 
 
-def sub_function1(S: set, G: nx.Graph):
+def sub_function1(S: set, G: nx.Graph):  # noqa
     """
         Input:
           - S: target set di nodi di G
@@ -28,7 +35,8 @@ def sub_function1(S: set, G: nx.Graph):
 
     return score
 
-def sub_function2(S: set, G: nx.Graph):
+
+def sub_function2(S: set, G: nx.Graph):  # noqa
     """
         Input:
           - S: target set di nodi di G
@@ -51,7 +59,7 @@ def sub_function2(S: set, G: nx.Graph):
     return score
 
 
-def sub_function3(S: set, G: nx.Graph):
+def sub_function3(S: set, G: nx.Graph):  # noqa
     """
         Input:
           - S: target set di nodi di G
@@ -74,14 +82,15 @@ def sub_function3(S: set, G: nx.Graph):
 
     return score
 
+
 def cost_seeds_greedy(
-    G: nx.Graph,
-    budget: Union[int, float],
-    cost_type: str,
-    sub_function: Callable,
-    initial_seed_set: Optional[Set] = None,
-    current_cost: Union[int, float] = 0
-):
+        G: nx.Graph,  # noqa
+        budget: Union[int, float],
+        cost_type: str,
+        sub_function: Callable,
+        initial_seed_set: Optional[Set] = None,
+        current_cost: Union[int, float] = 0  # noqa
+) -> Set[int]:
     """
         Input:
           - G: grafo non orientato (nx.Graph)
@@ -97,51 +106,52 @@ def cost_seeds_greedy(
         raise ValueError("sub_function must be one of the allowed sub_functions")
 
     remaining_nodes = set(G.nodes())
-    total_cost = current_cost
+    total_cost = current_cost  # noqa
+
     if initial_seed_set is None:
         S_selected = set()
     else:
         S_selected = initial_seed_set
         remaining_nodes -= initial_seed_set
 
-    pbar = tqdm(total=budget, desc="Cost Seeds Greedy progress")
+    # pbar = tqdm(total=budget, desc="Cost Seeds Greedy progress")
 
+    epsilon = 1e-6
     # Ciclo aggiunta nodi
-    while total_cost < budget and remaining_nodes:
-        # Miglior nodo dell'iterazione e il suo score
-        best_v = None
-        best_score = -float('inf')
+    with tqdm(total=budget, desc="Cost Seeds Greedy", unit="cost") as pbar:
+        while total_cost < budget and remaining_nodes:
+            best_v = None  # Miglior nodo dell'iterazione e il suo score
+            best_score = -float('inf')
 
-        # Valore della funzione submodulare del seed set alla attuale iterazione
-        current_value = sub_function(S_selected, G)
+            # Valore della funzione submodulare del seed set alla attuale iterazione
+            current_value = sub_function(S_selected, G)
 
-        # Ciclo per scegliere il nodo con lo score miglire
-        for v in remaining_nodes:
-            node_cost = G.nodes[v][cost_type]
+            # Ciclo per scegliere il nodo con lo score migliore
+            for v in remaining_nodes:
+                node_cost = G.nodes[v].get(cost_type, 0)
 
-            # value rappresenta lo score del nodo da confrontare con gli altri
-            gain = sub_function(S_selected | {v}, G) - current_value
-            value = gain / node_cost
+                # value rappresenta lo score del nodo da confrontare con gli altri
+                gain = sub_function(S_selected | {v}, G) - current_value
+                value = gain / (node_cost if node_cost != 0 else epsilon)
+                if value > best_score:
+                    best_score = value
+                    best_v = v
 
-            if value > best_score:
-                best_score = value
-                best_v = v
+            if best_v is None:
+                break
 
-        if best_v is None:
-            break
+            # Calcolo costo del nodo. Se tale costo fa superare il budget il nodo non viene aggiunto al seed set
+            node_cost = G.nodes[best_v].get(cost_type, 0)
+            if total_cost + node_cost > budget:
+                break
 
-        # Calcolo costo del nodo. Se tale costo fa superare il budget il nodo non viene aggiunto al seed set
-        node_cost = G.nodes[best_v][cost_type]
-        if total_cost + node_cost > budget:
-            break
+            total_cost += node_cost
+            pbar.update(node_cost)
+            S_selected.add(best_v)
+            remaining_nodes.remove(best_v)
 
-        total_cost += node_cost
-        S_selected.add(best_v)
-        remaining_nodes.remove(best_v)
-        pbar.update(node_cost)
-
-    pbar.close()
     return S_selected
+
 
 if __name__ == "__main__":
     G = nx.read_edgelist("../data/facebook_combined.txt", nodetype=int)
@@ -150,20 +160,21 @@ if __name__ == "__main__":
 
     # Configurazioni funzioni di costo e relative descrizioni
     cost_functions = {
-        "cost1": cost1,
-        "cost2": cost2,
+        # "cost1": cost1,
+        # "cost2": cost2,
         "cost3": cost3
     }
 
     descriptions = {
-        "cost1": "cost1: ceiling function of degree(v) / 2",
-        "cost2": "cost2: random int in [min(cost1), max(cost1)]",
+        # "cost1": "cost1: ceiling function of degree(v) / 2",
+        # "cost2": "cost2: random int in [min(cost1), max(cost1)]",
         "cost3": "cost3: scaled log10 of betweenness centrality"
     }
 
     for name, cost in cost_functions.items():
         algorithm_name = "CSG"
         cost_function_desc = descriptions[name]
+        desc = descriptions[name]
 
         # Calcolo range del budget
         min_budget = int(max(cost.values()))
@@ -180,15 +191,15 @@ if __name__ == "__main__":
             print(f"MinBudget = MaxBudget for {name}")
             continue
 
-        print(f"\n{name} — budget from {min_budget} to {max_budget}")
+        tqdm.write(f"\n{name} — budget da {min_budget} a {max_budget}")
 
-        current_seed_set = None
+        current_seed_set: Optional[Set[int]] = None
         current_cost = 0
 
         for budget_k in tqdm(
-                range(min_budget, max_budget + 1, 100),
-                desc=f"Budget loop for {name}",
-                unit="budget"
+            range(min_budget, max_budget + 1, 100),
+            desc=f"Budget loop for {name}",
+            unit="budget"
         ):
             start_time = time.time()
             S = cost_seeds_greedy(G, budget_k, name, sub_function1, current_seed_set, current_cost)
@@ -200,13 +211,11 @@ if __name__ == "__main__":
 
             exec_time = end_time - start_time
 
-            print(f"\nFunction: {name} | Budget: {budget_k}")
-            print(f"Target set size: {len(S)}")
-            print(f"Total cost: {total_cost}")
-            print(f"Execution time: {exec_time:.2f} seconds")
+            tqdm.write(f"Function: {name} | Budget: {budget_k}")
+            tqdm.write(f"Seed set size: {len(S)}; Total cost: {total_cost}; Time: {exec_time:.2f}s")
 
             log_experiment(
-                csv_path="./logs/prova_centrality.csv",
+                csv_path="./logs/cost3_CSG.csv",
                 algorithm_name=algorithm_name,
                 cost_function=cost_function_desc,
                 use_threshold=False,
